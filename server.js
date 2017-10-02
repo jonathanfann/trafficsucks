3a823310b8124886284672c1ff1445b81efadfbd
Jonathan Fann <jonathanfann@Jonathans-MacBook-Pro.local>
2017-10-01 20:55:25 -0400

    makitsoserverdontcrashonhome

--- server.js
+++ server.js
@@ -15,19 +15,34 @@ function getData(done) {
     })
 }
 
-function doThing(events) {
-    for (event in events) {
-        myHtml = myHtml.concat('<h2>' + event.head.id + '</h2>' +
-        '<h3>' + event.locations[0].description + '</h3>' +
-        '<p><strong' + event.locations[0].linkLocation.locationList[0].onAddress.county + ' County</strong></p>' +
-        '<p>' + event.head.eventType.type + '<br />' +
-        '<small>Started: ' + event.head.dates.started + '</small><br />' +
-        event.locations[0].description + '<br />')
-    }
-}
-
 app.get('/', function (req, res) {
-    requestGetTraffic();
+    baseUrl = trafficViewApiUrl + '&system=SCDOT&format=rf-json';
+    console.log(req.params);
+    getData(function(err, data) {
+        var myObj = JSON.parse(data.body);
+        var myHtml = '<html><head><link href="//fonts.googleapis.com/css?family=Eczar" \
+        rel="stylesheet"><style>html,body,h1,h2,h3,h4,h5,p,strong,small {font-family:"Eczar" !important; } \
+        html,body { margin:0; padding:0; background:#eeeeee; color:#333333; } h1 { font-size: 40px;}</style> \
+        <meta name="viewport" content="width=device-width, initial-scale=1"></head> \
+        <body><div style="background:#ffffff; padding:10px 20px;"> \
+        <h1>SCDOT Events &amp; Incidents</h1></div><div style="border-top:1px dotted red; padding:20px;">';
+        var events = myObj.trafficEvents.events;
+        res.write(myHtml);
+        for (event = 0; event < events.length; event++) {
+            console.log(events[event]);
+            res.write(
+                '<h2 id="' + events[event].head.id + '">' + events[event].head.id + '</h2>' +
+                '<p><strong>' + events[event].head.name + '</strong></p>' +
+                '<p>' + events[event].locations[0].description + '</p>' +
+                '<p>' + events[event].head.eventType.type + '</p>' +
+                '<p><small>Started: ' + events[event].head.dates.started + '</small><br/></p>'
+            )
+            if (event >= (events.length - 1)) {
+                res.write('<hr style="margin-top:20px; height:1px; border:0; background:#d3d3d3; margin-bottom:20px;" /><p><small>&copy; 2017 Jonathan Fann</small></p><p><small>This project is currently under development</small></p></div></body></html>')
+                res.end();
+            }
+        }
+    })
 })
 
 // app.get('/system/:systemId', function (req, res) {
